869ae42e93bf03a994c574716611e9566e4a065f
mjnwnabw174-byte <mjnwnabw174@gmail.com>
2026-07-15 23:04:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,11 @@
 import telebot
-import os
 from handlers import bot_handlers
-from flask import Flask, request
 from config import TOKEN
 
 bot = telebot.TeleBot(TOKEN)
-app = Flask(__name__)
 
-@app.route('/' + TOKEN, methods=['POST'])
-def get_message():
-    json_str = request.stream.read().decode('utf-8')
-    update = telebot.types.Update.de_json(json_str)
-    bot.process_new_updates([update])
-    return "!", 200
+# إعداد المعالجات
+bot_handlers(bot)
 
-if __name__ == "__main__":
-    bot_handlers(bot)
-    bot.remove_webhook()
-    bot.set_webhook(url="https://italian-bot-final.onrender.com/" + TOKEN)
-    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+print("الوحش يعمل الآن...")
+bot.infinity_polling()
