1242258ba71642856a0f9d935d29248e440818cb
mjnwnabw174-byte <mjnwnabw174@gmail.com>
2026-07-15 20:35:03 +0300

    Update logic.py

--- logic.py
+++ logic.py
@@ -2,11 +2,8 @@ import requests
 from config import WEB_APP_URL
 
 def add_user_to_sheet(user, link):
-    # إرسال بيانات المستخدم لـ Google Sheets
     params = {'action': 'addAccount', 'link': link, 'user': user}
     requests.get(WEB_APP_URL, params=params)
 
 def get_next_user():
-    # هنا سنضع لاحقاً كود جلب الحساب التالي من الجدول
     return "سيتم تفعيل رابط التبادل قريباً!"
-
