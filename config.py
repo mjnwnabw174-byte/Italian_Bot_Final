6882364616d6f135b927865af6661fab7e66e940
mjnwnabw174-byte <mjnwnabw174@gmail.com>
2026-07-16 22:43:44 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-TOKEN = '8877859402:AAESVv6dFFHoSwni-WqcW2jTqNRlCDRYyo8'
+TOKEN = '8877859402:AAFIrW1qF4BMGVC9BzuMsicza2pA-Exm-DE'
 WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbywjm4v3ExOL1ynBonQk1hYVzCzoeMUKvPSRTLXoi6cRBYUncUEsNgZ-NZEqPEPMwBu/exec'
 MY_ID = '7068860200'
